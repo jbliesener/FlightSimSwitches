557d9d8416c243c370bed33b687b70c716a176fd
Jorg Neves Bliesener <jbliesener@bliesener.com>
2019-01-13 18:06:12 -0200

    Updated for simpler library intialization

---
+++ examples/Demo_Callbacks/Demo_Callbacks.ino
@@ -0,0 +1,43 @@
+#include <FlightSimSwitches.h>
+
+FlightSimSwitches switches;
+FlightSimWriteDatarefSwitch switch1(3,SWITCH_POSITIONS(0,1,2),SWITCH_VALUES(1,2,3));
+FlightSimOnOffCommandSwitch switch2(3);
+FlightSimOnOffDatarefSwitch switch3(4, true); // inverted values!
+
+// This callback function will be called whenever switch 1 changes
+void sw1ChangeFunction(float value) {
+  Serial.print("Switch 1 changed to ");
+  Serial.println(switch1.getValue());
+}
+
+// This callback function will be called whenever switch 2 or 3 changes.
+// The "context" parameter is the pin number
+void sw23ChangeFunction(float value, void* context) {
+  uint32_t pinNumber = (uint32_t) context;
+  Serial.print("Pin ");
+  Serial.print(pinNumber);
+  Serial.print(" changed to ");
+  Serial.println(value);
+}
+
+void setup() {
+  delay(1000);
+  Serial.println("Callback demo");
+
+  switch1 = XPlaneRef("put/dataref/here");
+  switch2.setOnOffCommands(
+    XPlaneRef("put/the/on/command/here"), 
+    XPlaneRef("put/the/off/command/here"));
+  switch3 = XPlaneRef("put/another/dataref/here");
+  switch1.onChange(sw1ChangeFunction);
+  switch2.onChange(sw23ChangeFunction,(void*) 3); // Switch 2 is on pin 3
+  switch3.onChange(sw23ChangeFunction,(void*) 4); // Switch 3 is on pin 4
+
+  switches.begin();
+}
+
+void loop() {
+  FlightSim.update();
+  switches.loop();
+}
--- examples/Demo_Cessna172/Demo_Cessna172.ino
+++ examples/Demo_Cessna172/Demo_Cessna172.ino
@@ -19,34 +19,27 @@
 //                   - Pin 9 to GND: Magnetos L
 //                   - Pin 10 to GND: Magnetos BOTH
 //                   - Pin 11 to GND: Magnetos START  (DOES NOT WORK YET IN THIS VERSION)
-//
-// We therefore have the following list of input pins: 2, 5, 8, 9, 10 and 11.
-// The first input pin at position 0 of this list is pin 2. The second pin
-// at position 1 is pin number 5. Pins 8 to 11 are at positions 2-5 of this
-// list.
-//
+
 // You can follow the actions of this Sketch on the serial console
 
 #include <FlightSimSwitches.h>
 
 // Declaring the FlightSimSwitches object. This object must always be
 // declared BEFORE any specific switch. We can either specify all
-// parameters here or configure them later in setup()
-// In this case, we specify that we have the six input pins mentioned
-// above.
-FlightSimSwitches switches(6,SWITCH_PINS(2,5,8,9,10,11));
+// parameters here or configure them later in setup().
+// In this case, we use the object in its standard configuration,
+// without any parameters.
+FlightSimSwitches switches;
 
 // Now we configure our individual switches
-// The first switch is the taxi light switch. We use the pin POSITION
-// in the SWITCH_PINS to specify which pin we use for that switch. DO NOT
-// USE THE DIRECT PIN NUMBER HERE!!
-// Pin POSITION 0 is the first pin in the array, wich is pin NUMBER 2
-FlightSimOnOffCommandSwitch taxiLightSwitch(0);
+// The first switch is the taxi light switch on pin 2. X-Plane
+// uses two commands to handle this switch: One when the switch
+// turns on, another one when it turns off. We therefore use a
+// FlightSimOnOffCommandSwitch object.
+FlightSimOnOffCommandSwitch taxiLightSwitch(2);
 
-// Configure the landing lights the same way
-// Pin POSITION 1 is the SECOND pin in the list of input pins, which
-// is pin NUMBER 5 on the Teensy.
-FlightSimOnOffCommandSwitch landingLightSwitch(1);
+// Configure the landing lights on pin 5 the same way.
+FlightSimOnOffCommandSwitch landingLightSwitch(5);
 
 // The Magnetos switch is more complex. We have a switch with four
 // positions and X-Plane wants "up" and "down" commands in order to
@@ -57,12 +50,14 @@ FlightSimOnOffCommandSwitch landingLightSwitch(1);
 // 1 (R), 2 (L), 3 (BOTH) and 4 (START). The fifth position corresponds
 // to dataref value 0 and therefore does not need to be specified
 // explicitly.
-FlightSimUpDownCommandSwitch magnetosSwitch(4,SWITCH_POSITIONS(2,3,4,5),SWITCH_VALUES(1,2,3,4));
-
+//                                             +--- pin numbers             +-- value for each pin
+//                                             |                            |   number
+//                                             v                            v
+FlightSimUpDownCommandSwitch magnetosSwitch(4,SWITCH_POSITIONS(8,9,10,11),SWITCH_VALUES(1,2,3,4));
 
 void setup() {
   // Serial output stuff, see Wiki (https://www.github.com/jbliesener/FlightSimSwitches/wiki/Serial)
-  delay(2000);         // Teensy startup delay
+  delay(1000);         // Teensy startup delay
   Serial.begin(9600);
   FlightSim.update();  // Call FlightSim.update to make serial output work. Must not take longer
                        // than 2 seconds for the next call to FlightSim.update()
@@ -105,5 +100,4 @@ void setup() {
 void loop() {
   FlightSim.update();
   switches.loop();
-
 }
